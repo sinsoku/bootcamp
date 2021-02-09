54a0f490f4b2eafa1e0250d65e26a20942b75c00
komagata <komagata@gmail.com>
2021-02-10 01:10:36 +0900

    参考書籍をAmazonECSを使わない方式に変更
    AmazonECSのAPIがほぼ使えなくなったので。

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -57,7 +57,7 @@ class PracticesController < ApplicationController
       :include_progress,
       :memo,
       category_ids: [],
-      reference_books_attributes: %i[id title asin _destroy]
+      reference_books_attributes: %i[id title price page_url cover _destroy]
     )
   end
 
