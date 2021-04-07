537448e202208b9aad94fd2cd97aaa3591585e19
harukus <43bw.hana@gmail.com>
2021-04-07 14:45:49 +0900

    インデント整形

--- app/javascript/user_mentor_memo.vue
+++ app/javascript/user_mentor_memo.vue
@@ -1,52 +1,52 @@
 <template lang="pug">
-.is-memo.is-only-mentor
-  section.a-card(v-if="!editing")
-    header.card-header.is-sm
-      h2.card-header__title
-        | メンター向けユーザーメモ
-    .card-body
-      .js-target-blank.is-long-text(v-html="markdownMemo")
-    footer.card-footer
-      .card-footer-actions
-        button.card-footer-actions__action.a-button.is-md.is-primary.is-block(
-          @click="editMemo"
+  .is-memo.is-only-mentor
+    section.a-card(v-if="!editing")
+      header.card-header.is-sm
+        h2.card-header__title
+          | メンター向けユーザーメモ
+      .card-body
+        .js-target-blank.is-long-text(v-html="markdownMemo")
+      footer.card-footer
+        .card-footer-actions
+          button.card-footer-actions__action.a-button.is-md.is-primary.is-block(
+            @click="editMemo"
+          )
+            i.fas.fa-pen
+            | 編集
+    .thread-comment-form__form.a-card(v-show="editing")
+      .thread-comment-form__tabs.js-tabs
+        .thread-comment-form__tab.js-tabs__tab(
+          :class="{ 'is-active': isActive('memo') }",
+          @click="changeActiveTab('memo')"
         )
-          i.fas.fa-pen
-          | 編集
-  .thread-comment-form__form.a-card(v-show="editing")
-    .thread-comment-form__tabs.js-tabs
-      .thread-comment-form__tab.js-tabs__tab(
-        :class="{ 'is-active': isActive('memo') }",
-        @click="changeActiveTab('memo')"
-      )
-        | メモ
-      .thread-comment-form__tab.js-tabs__tab(
-        :class="{ 'is-active': isActive('preview') }",
-        @click="changeActiveTab('preview')"
-      )
-        | プレビュー
-    .thread-comment-form__markdown-parent.js-markdown-parent
-      .thread-comment-form__markdown.is-editor.js-tabs__content(
-        :class="{ 'is-active': isActive('memo') }"
-      )
-        textarea.a-text-input.js-warning-form.thread-comment-form__textarea(
-          :id="`js-user-mentor-memo`",
-          data-preview="#user-mentor-memo-preview",
-          v-model="memo",
-          name="user[memo]"
+          | メモ
+        .thread-comment-form__tab.js-tabs__tab(
+          :class="{ 'is-active': isActive('preview') }",
+          @click="changeActiveTab('preview')"
         )
-      .thread-comment-form__markdown.is-preview.js-tabs__content(
-        :class="{ 'is-active': isActive('preview') }"
-      )
-        .is-long-text.thread-comment-form__preview(v-html="markdownMemo")
-    .card-footer
-      .thread-comment-form__actions
-        .thread-comment-form__action
-          button.a-button.is-md.is-warning.is-block(@click="updateMemo")
-            | 保存する
-        .thread-comment-form__action
-          button.a-button.is-md.is-secondary.is-block(@click="cancel")
-            | キャンセル
+          | プレビュー
+      .thread-comment-form__markdown-parent.js-markdown-parent
+        .thread-comment-form__markdown.is-editor.js-tabs__content(
+          :class="{ 'is-active': isActive('memo') }"
+        )
+          textarea.a-text-input.js-warning-form.thread-comment-form__textarea(
+            :id="`js-user-mentor-memo`",
+            data-preview="#user-mentor-memo-preview",
+            v-model="memo",
+            name="user[memo]"
+          )
+        .thread-comment-form__markdown.is-preview.js-tabs__content(
+          :class="{ 'is-active': isActive('preview') }"
+        )
+          .is-long-text.thread-comment-form__preview(v-html="markdownMemo")
+      .card-footer
+        .thread-comment-form__actions
+          .thread-comment-form__action
+            button.a-button.is-md.is-warning.is-block(@click="updateMemo")
+              | 保存する
+          .thread-comment-form__action
+            button.a-button.is-md.is-secondary.is-block(@click="cancel")
+              | キャンセル
 </template>
 
 <script>
